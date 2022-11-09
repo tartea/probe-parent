ebbaf94e94f6d5118a3027635a91931126a9c8b2
tartea <1262466460@qq.com>
2022-11-09 22:54:09 +0800

    fix:修改mysql的输出格式

--- probe-agent/src/main/java/org/tartea/plugin/monitor/MysqlMonitor.java
+++ probe-agent/src/main/java/org/tartea/plugin/monitor/MysqlMonitor.java
@@ -1,5 +1,6 @@
 package org.tartea.plugin.monitor;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.ReflectUtil;
@@ -9,6 +10,7 @@ import org.tartea.plugin.util.ConsoleInfoUtil;
 
 import java.lang.reflect.Method;
 import java.util.Date;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 
 /**
@@ -27,21 +29,48 @@ public class MysqlMonitor {
             resObj = callable.call();
             return resObj;
         } finally {
-
-            String originalSql = ReflectUtil.invoke(obj, "getPreparedSql");
-            if (StrUtil.isNotBlank(originalSql)) {
-                System.out.println(originalSql.contains("\n\r"));;
+            if (Objects.equals(obj.getClass().getSimpleName(), "PreparedStatement")) {
+                buildPreparedStatementSql(obj);
+            } else if (Objects.equals(obj.getClass().getSimpleName(), "ClientPreparedStatement")) {
+                buildClientPreparedStatement(obj);
             }
 
-            String replaceSql = ReflectUtil.invoke(obj, "asSql");
-            ConsoleInfoUtil.consoleInfo
-                    .appendLog("数据库名称：Mysql")
-                    .appendLog("线程ID：" + Thread.currentThread().getId())
-                    .appendLog("时间：" + DateUtil.format(new Date(), DatePattern.NORM_DATETIME_MS_PATTERN))
-                    .appendLog("原始SQL: ")
-                    .appendLog(originalSql)
-                    .appendLog("替换SQL：")
-                    .appendLog(replaceSql);
         }
     }
+
+
+    private static void buildPreparedStatementSql(Object obj) {
+        String originalSql = (String) BeanUtil.getFieldValue(obj, "originalSql");
+        String replaceSql = ReflectUtil.invoke(obj, "asSql");
+
+        sqlInfo(originalSql, replaceSql);
+    }
+
+
+    private static void buildClientPreparedStatement(Object obj) {
+        String originalSql = ReflectUtil.invoke(obj, "getPreparedSql");
+
+
+        String replaceSql = ReflectUtil.invoke(obj, "asSql");
+
+        sqlInfo(originalSql, replaceSql);
+    }
+
+    private static void sqlInfo(String originalSql, String replaceSql) {
+        if (StrUtil.isNotBlank(originalSql)) {
+            originalSql = originalSql.replace("\n", " ").replace("\r"," ").replace("\t"," ");
+        }
+        if (StrUtil.isNotBlank(replaceSql)) {
+           replaceSql =  replaceSql.replace("\n", " ").replace("\r"," ").replace("\t"," ");
+        }
+        ConsoleInfoUtil.consoleInfo
+                .appendLog("数据库名称：Mysql")
+                .appendLog("线程ID：" + Thread.currentThread().getId())
+                .appendLog("时间：" + DateUtil.format(new Date(), DatePattern.NORM_DATETIME_MS_PATTERN))
+                .appendLog("原始SQL: ")
+                .appendLog(originalSql)
+                .appendLog("替换SQL：")
+                .appendLog(replaceSql);
+    }
+
 }
